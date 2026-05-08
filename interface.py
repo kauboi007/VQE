7410de661e3e5002838120e915e62e0e6aef2174
kauboi007 <kaushikmugundan2019@gmail.com>
2026-05-08 12:40:49 +0530

    added multistart cobyla

--- interface.py
+++ interface.py
@@ -10,12 +10,14 @@ coeff=[-0.8105,0.1721,-0.1721,- 0.2227,0.1209,0.1209]#pauli coeff as an array
 def costfn(t):
     res = qs.eval(f"mkvqe.measureall({[float(x) for x in t]}, 1000)")
     return coeff[0]+res[0]*coeff[1]+res[1]*coeff[2]+res[2]*coeff[3]+res[3]*coeff[4]+res[4]*coeff[5]
-
-t0=np.array([0.1,0.2,0.3,0.4])#initial angles - can be random
-
-result=minimize(costfn,t0,method="COBYLA") #optimizer - uses cobyla 
-
-print(f"Ground state energy in Hatrees:{result.fun}")
-print(f"Ground state energy in KJ/mol:{result.fun*2625.4996}")
-fp=result.x # final parameters array
+best=None;
+for i in range(5):
+    t0 = np.random.uniform(0, 2*np.pi, 4)
+    res = minimize(costfn, t0, method='COBYLA')
+    if best is None or res.fun<best.fun:
+        best=res
+    
+print(f"Ground state energy in Hatrees:{best.fun}")
+print(f"Ground state energy in KJ/mol:{best.fun*2625.4996}")
+fp=best.x # final parameters array
 print(f"Final parameters: t1={fp[0]} t2={fp[1]} t3={fp[2]} t4={fp[3]}")
